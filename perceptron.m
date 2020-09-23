60e5e92378cd6ec7254e3935e41c65c6bc5b6958
Benediktas <bcicenas@gmail.com>
2020-09-23 20:26:42 +0300

    single perceptron

--- perceptron.m
+++ perceptron.m
@@ -1,20 +1,21 @@
 %Init variables
 INPUT = readmatrix('Data.txt');
+INPUT2 = readmatrix('Data2.txt');
 h = size(INPUT, 1);
-
+h2 = size(INPUT2, 1);
 w1 = randn(1);
 w2 = randn(1);
 b = randn(1);
-n = 50;
+n = 100;
 nu = 0.3;
 y = 0;
 
 for i = 1:n
-    E = zeros(1, h);
-    for j = 1:h
-        X1 = INPUT(j,1);
-        X2 = INPUT(j,2);
-        T = INPUT(j,3);
+    E = zeros(1, h2);
+    for j = 1:h2
+        X1 = INPUT2(j,1);
+        X2 = INPUT2(j,2);
+        T = INPUT2(j,3);
         %calculate function
         f = X1*w1 + X2*w2 + b;
         %check sign
@@ -25,10 +26,10 @@ for i = 1:n
     end
     
     %update w1, w2 and b
-    for j = 1:h
-        X1 = INPUT(j,1);
-        X2 = INPUT(j,2);
-        T = INPUT(j,3);
+    for j = 1:h2
+        X1 = INPUT2(j,1);
+        X2 = INPUT2(j,2);
+        T = INPUT2(j,3);
         
         w1 = w1 + nu*E(j)*X1;
         w2 = w2 + nu*E(j)*X2;
@@ -43,8 +44,9 @@ disp(b);
 
 % test 
 TEST = readmatrix('test.txt');
-ANS = zeros(1, h);
-for i = 1:h
+ht = size(TEST, 1);
+ANS = zeros(1, ht);
+for i = 1:ht
    y = TEST(i,1)*w1 + TEST(i,2)*w2 + b; 
    ANS(i) = sign(y);
 end
